c2da765be1d80fa02fbf8369491a0f1b1f72bb99
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-18 14:24:31 +0530

    #include<bits/stdc++.h> #include<math.h> int main() {     int n;     float s;     std::cin>>n;     s=sqrt(n);     if(s>(int)s)        std::cout<<"False";     else        std::cout<<"True";
        return 0;
    }

---
+++ perfect_square_root_or_not.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<math.h>
+int main()
+{
+    int n;
+    float s;
+    std::cin>>n;
+    s=sqrt(n);
+    if(s>(int)s)
+       std::cout<<"False";
+    else
+       std::cout<<"True";
+    
+    return 0;
+}
