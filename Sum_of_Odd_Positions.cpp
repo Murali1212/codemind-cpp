fba254defdabcb70bf9c68dfb87110cffc07a08f
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-18 15:28:51 +0530

    #include<bits/stdc++.h> int main() {     int n,i,o_sum=0,x[100];     std::cin>>n;
        for(i=0;i<n;i++)
         { 
            std::cin>>x[i];
            if(i%2!=0)
              o_sum+=x[i];
         }
        
          std::cout<<o_sum;
        return 0;
    }

---
+++ Sum_of_Odd_Positions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,i,o_sum=0,x[100];
+    std::cin>>n;
+
+    for(i=0;i<n;i++)
+     { 
+        std::cin>>x[i];
+        if(i%2!=0)
+          o_sum+=x[i];
+     }
+    
+      std::cout<<o_sum;
+    return 0;
+}
