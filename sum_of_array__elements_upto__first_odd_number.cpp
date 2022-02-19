ea69cca2f2e2f2bec44b5d62ea75ea1782aeb845
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-19 14:43:53 +0530

    #include<bits/stdc++.h> int main() {     int n,x[10],k,i,sum=0;     std::cin>>n;     for(i=0;i<n;i++)           std::cin>>x[i];     std::cin>>k;     for(i=0;i<n;i++)        if(x[i]%2!=0)          break;        else           sum=sum+x[i];
        std::cout<<sum;
        return 0;
    }

---
+++ sum_of_array__elements_upto__first_odd_number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,x[10],k,i,sum=0;
+    std::cin>>n;
+    for(i=0;i<n;i++)
+          std::cin>>x[i];
+    std::cin>>k;
+    for(i=0;i<n;i++)
+       if(x[i]%2!=0)
+         break;
+       else
+          sum=sum+x[i];
+    
+    std::cout<<sum;
+    return 0;
+}
