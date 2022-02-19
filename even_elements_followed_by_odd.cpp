03b408ddb8db8ee365eeb5bf32191b5cd7bcdf95
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-19 11:01:28 +0530

    #include<bits/stdc++.h> int main() {     int x[10],n,even[10],odd[10],i,j=0,k=0;     std::cin>>n;     for(i=0;i<n;i++)      {          std::cin>>x[i];          if(x[i]%2==0)            even[j++]=x[i];          else            odd[k++]=x[i];      }      for(i=0;i<j;i++)        std::cout<<even[i]<<" ";      for(i=0;i<k;i++)       std::cout<<odd[i]<<" ";      return 0;
    }

---
+++ even_elements_followed_by_odd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int x[10],n,even[10],odd[10],i,j=0,k=0;
+    std::cin>>n;
+    for(i=0;i<n;i++)
+     {
+         std::cin>>x[i];
+         if(x[i]%2==0)
+           even[j++]=x[i];
+         else
+           odd[k++]=x[i];
+     }
+     for(i=0;i<j;i++)
+       std::cout<<even[i]<<" ";
+     for(i=0;i<k;i++)
+      std::cout<<odd[i]<<" ";
+     return 0;
+
+}
