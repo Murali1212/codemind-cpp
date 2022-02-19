71a1e623325677f29006e68bb73cf93f742800d0
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-19 14:50:15 +0530

    #include<bits/stdc++.h> int main() {     int n,x[10],k,i,sum1=0,sum2=0;     std::cin>>n;     for(i=0;i<n;i++)     {         std::cin>>x[i];           if(i<n/2)             sum1+=x[i];           else             sum2+=x[i];     }
        std::cout<<sum1<<"
    "<<sum2;
        return 0;
    }

---
+++ sum_of_first_half_and_second_half_in_an_array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,x[10],k,i,sum1=0,sum2=0;
+    std::cin>>n;
+    for(i=0;i<n;i++)
+    {
+        std::cin>>x[i];
+          if(i<n/2)
+            sum1+=x[i];
+          else
+            sum2+=x[i];
+    }
+    
+    std::cout<<sum1<<"
+"<<sum2;
+    return 0;
+}
