793b2d4c3266415e660a960350c6295302356293
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-19 10:21:14 +0530

    #include<bits/stdc++.h> int main() {     int n,x[20],count=0,i;     std::cin>>n;     for(i=0;i<n;i++)       std::cin>>x[i];
         for(i=0;i<n-2;i++)
         {
            
               
              
            if(x[i]%2!=0 && x[i+1]%2!=0 && x[i+2]%2!=0)
            {
                count++;
                          
            }
              
             
         }
        std::cout<<count;
         return 0;
    }

---
+++ count_the_add_numbers_inbetween_odd_numbers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,x[20],count=0,i;
+    std::cin>>n;
+    for(i=0;i<n;i++)
+      std::cin>>x[i];
+      
+     for(i=0;i<n-2;i++)
+     {
+        
+           
+          
+        if(x[i]%2!=0 && x[i+1]%2!=0 && x[i+2]%2!=0)
+        {
+            count++;
+                      
+        }
+          
+         
+     }
+    std::cout<<count;
+     return 0;
+}
