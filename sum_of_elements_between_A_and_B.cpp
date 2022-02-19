48310333e85d94f20fa76003dee058595bc88061
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-19 14:30:04 +0530

    #include<bits/stdc++.h> int main() {     int n,x[40],i,a,b,sum=0;     std::cin>>n;     for(i=0;i<n;i++)       std::cin>>x[i];     std::cin>>a>>b;
        for(i=0;i<n;i++)
        {
            if(x[i]>=a && x[i]<=b)
                sum=sum+x[i];
        }
        
        std::cout<<sum;
        return 0;
    }

---
+++ sum_of_elements_between_A_and_B.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,x[40],i,a,b,sum=0;
+    std::cin>>n;
+    for(i=0;i<n;i++)
+      std::cin>>x[i];
+    std::cin>>a>>b;
+    
+    for(i=0;i<n;i++)
+    {
+        if(x[i]>=a && x[i]<=b)
+            sum=sum+x[i];
+    }
+    
+    std::cout<<sum;
+    return 0;
+}
