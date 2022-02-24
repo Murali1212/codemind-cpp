0700ea5eb9b0487b98eeda204374535ed9fdacdb
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-24 15:41:52 +0530

    #include<bits/stdc++.h> int main() {   int n,x[30],k,count=0,i;   std::cin>>n>>k;   for(i=0;i<n;i++)     std::cin>>x[i];   for(i=0;i<n;i++)   {       if(x[i]%k==0)         count++;   }  std::cout<<count;   return 0; }

---
+++ Count_elements_divisible_by_k.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+int main()
+{
+  int n,x[30],k,count=0,i;
+  std::cin>>n>>k;
+  for(i=0;i<n;i++)
+    std::cin>>x[i];
+  for(i=0;i<n;i++)
+  {
+      if(x[i]%k==0)
+        count++;
+  }
+ std::cout<<count;
+  return 0;
+}
